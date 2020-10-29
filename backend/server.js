81b1a6d42fcfb8f69eca2d7648a358d9315d1f87
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2020-10-28 23:14:59 -0400

    front end and back end

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 //entry point for our server
 
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -8,4 +12,20 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.listen(5000, console.log('server running on 5000'))
+//getting all products
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+//getting indivdual products
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
