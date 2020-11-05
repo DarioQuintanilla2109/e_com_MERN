2e730de519c2338dfae929083cfaedee44037ed5
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2020-11-05 00:59:31 -0500

    added backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddlewear.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -12,20 +17,17 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-//getting all products
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+//using our routes in productRoutes.js file
+app.use('/api/products', productRoutes)
 
-//getting indivdual products
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
